fd63b4fac103add55dafe56d3777e2cf4b949db4
Jesse Heckman <j.heckman@science.ru.nl>
2018-09-05 10:50:28 +0200

    removed obsolete functions/added pause function

--- utilities/housekeeping/pb_pause.m
+++ utilities/housekeeping/pb_pause.m
@@ -1,19 +1,15 @@
-function ledArray = pb_vLedLookup(ledArray)
-% PB_VLEDLOOKUP()
+function pb_pause(msg)
+% PB_PAUSE(msg)
 %
-% PB_VLEDLOOKUP()  ...
+% PB_PAUSE() allows you to pause with display msg until any key is pressed.
 %
-% See also ...
+% See also PAUSE, INPUT
 
 % PBToolbox (2018): JJH: j.heckman@donders.ru.nl
 
-   if nargin == 0; return; end
-   tmp = ledArray;
-   
-   for i = numel(ledArray)
-      tmp(i) = lookup();
-   end
- 
+   if nargin == 0; pause; return; end
+   disp(msg);
+   pause;
 end
  
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
