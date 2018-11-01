ba7b5da45c311ddb4207314c7d90423c798d4bc4
Jesse Heckman <j.heckman@science.ru.nl>
2018-11-01 09:42:50 +0100

    update double sac

---
+++ utilities/fileio/backup/pb_makeglobal.m
@@ -0,0 +1,31 @@
+function pb_makeglobal(varargin)
+% PB_MAKEGLOBAL()
+%
+% PB_MAKEGLOBAL()  ...
+%
+% See also ...
+
+% PBToolbox (2018): JJH: j.heckman@donders.ru.nl
+
+   
+   %% Initialize
+   
+   init = pb_keyval(init,varargin,false)
+   
+   % read backup_dir: if empty, fill pb_getdir
+   
+   % - set system
+   
+   %% Connect to server
+   
+   
+   
+end
+ 
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%                                                           %
+%       Part of Programmeer Beer Toolbox (PBToolbox)        %
+%       Written by: Jesse J. Heckman (2018)                 %
+%                                                           %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
---
+++ utilities/fileio/backup/pb_makelocal.m
@@ -0,0 +1,19 @@
+function pb_makelocal()
+% PB_MAKELOCAL()
+%
+% PB_MAKELOCAL()  ...
+%
+% See also ...
+
+% PBToolbox (2018): JJH: j.heckman@donders.ru.nl
+
+   
+end
+ 
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%                                                           %
+%       Part of Programmeer Beer Toolbox (PBToolbox)        %
+%       Written by: Jesse J. Heckman (2018)                 %
+%                                                           %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
