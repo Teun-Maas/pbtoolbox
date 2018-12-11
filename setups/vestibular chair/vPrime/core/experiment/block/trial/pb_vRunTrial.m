e689d0cb5ee63b75be9caf7a6a780835f079d072
Jesse Heckman <j.heckman@science.ru.nl>
2018-12-11 13:49:09 +0100

    fixed runtrial memory leak

--- setups/vestibular chair/vPrime/core/experiment/block/trial/pb_vRunTrial.m
+++ setups/vestibular chair/vPrime/core/experiment/block/trial/pb_vRunTrial.m
@@ -11,17 +11,12 @@ function pb_vRunTrial(zbus, trialdur)
    zbus.zBusTrigA(0, 0, 2); % reset, clock start, (0,0,2): trigger entire rack, with a pulse structure, and 2 ms delay(2 ms = minimum).
 
    %% Trigger event 1
-   % cfg.zBus.zBusTrigB(0, 0, 2); % start event 1, trial onset
    zbus.zBusTrigB(0, 0, 2); % start event 1/trial onset; trigger zBus 4 = RA16;
-
-   %% Button Press
-   %   disp('Waiting for RZ6 button press/sound/led/acquisition');
 
    t = tic;
    while toc(t) < trialdur
-   % 	disp('waiting')
-      % do nothing
-   end % sound is played after this loop exits.
+      pause(0.05);
+   end
 end
  
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
