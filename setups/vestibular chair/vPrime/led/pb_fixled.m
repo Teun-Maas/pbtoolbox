64718b399b96b118af1200d66c27c5d9e8fa09ba
Jesse Heckman <j.heckman@science.ru.nl>
2020-07-08 11:37:24 +0200

    update minor fix fixled

--- setups/vestibular chair/vPrime/led/pb_fixled.m
+++ setups/vestibular chair/vPrime/led/pb_fixled.m
@@ -1,4 +1,4 @@
-function [leds,s] = pb_fixled
+function [leds,s] = pb_fixled(varargin)
 % PB_LIGHTWARNING()
 %
 % PB_LIGHTWARNING()  ...
@@ -7,20 +7,19 @@ function [leds,s] = pb_fixled
 
 % PBToolbox (2019): JJH: j.heckman@donders.ru.nl
 
-   import org.zeromq.ZMQ
+   target = pb_keyval('led',varargin,5);
    
+   import org.zeromq.ZMQ
    leds  = ledcontroller_pi('dcn-led06','dcn-led07','dcn-led09','dcn-led10');
    
    n     = 2;
    s     = ledpattern(n);
-   central_speaker = 10; % central led = 10 (11th starting with 0 from the left)
+   central_speaker = 5; % central led = 5 (6th starting with 0 from the left)
    for iC = 1:n
       if mod(iC,2) == 1
          s(iC).set(central_speaker,'r');
       end
        s(iC).intensity('r', 50);
    end
-
    leds.write(s);
-
 end
