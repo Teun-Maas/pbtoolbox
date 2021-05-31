f6b03682e4ccc9c822dffa7d98c1cd54f681b387
Jesse Heckman <j.heckman@science.ru.nl>
2021-05-31 13:28:27 +0200

    lookup

--- setups/vestibular chair/vPrime/expfile/pb_vWriteHeader.m
+++ setups/vestibular chair/vPrime/expfile/pb_vWriteHeader.m
@@ -1,42 +1,43 @@
-function pb_vWriteHeader(fid,datdir,ITI,Nblcks,Ntrls,Rep,Rnd,Mtr,Stim,varargin)
-% PB_VWRITEHEADER()
+function pb_vWriteHeader(fid,nblocks,ntrials,GV)
+% PB_VWRITEHEADER
 %
-% PB_VWRITEHEADER()  ...
+% PB_VWRITEHEADER(fid, GV) writes expfile headers for vPrime setup
 %
 % See also ...
 
-% PBToolbox (2018): JJH: j.heckman@donders.ru.nl
-
-   lab = keyval('Lab',varargin);
-   if isempty(lab)
-      lab = 1; % hoop by default
-   end
+% PBToolbox (2021): JJH: j.heckman@donders.ru.nl
+   
    % Header of exp-file
    fprintf(fid,'%s\n','%');
-   fprintf(fid,'%s\n',['%% Experiment: C:\DATA\' datdir]);
+   fprintf(fid,'%s\n',['%% Experiment: C:\DATA\' GV.datdir]);
    fprintf(fid,'%s\n','%');
    fprintf(fid,'\n');
    fprintf(fid,'%s %s\n','%%','HEADER');
    
-   fprintf(fid,'%s\t\t%d\t%d\n','ITI',ITI(1),ITI(2));
-   fprintf(fid,'%s\t%d\n','Blocks',Nblcks);
-   fprintf(fid,'%s\t%d\n','Trials',Ntrls);
-   fprintf(fid,'%s\t%d\n','Repeats',Rep);
-   fprintf(fid,'%s\t%d\t%s\n','Random',Rnd,'% 0=no, 1=per set, 2=all trials');
-   fprintf(fid,'%s\t\t%s\n','Motor',Mtr);
-   if lab>1
-      fprintf(fid,'%s\t\t%d\n','Lab',lab);
-   end
-   fprintf(fid,'%s\t\t%s\n','Stim',Stim);
-   fprintf(fid,'\n');
-   fprintf(fid,'%s %s\t%s\t%s\t%s\t%s\n','%','AX','SIG','AMP','DUR','FREQ');
-   fprintf(fid,'%s\n','%');
+   fprintf(fid,'%s\t\t%d\t%d\n','ITI', GV.ITI(1), GV.ITI(2));
+   fprintf(fid,'%s\t%d\n','Blocks', nblocks);
+   fprintf(fid,'%s\t%d\n','Trials', ntrials);
+   fprintf(fid,'%s\t\t%d\n','Lab',GV.lab);
+   fprintf(fid,'%s\t%d\n','Stim',GV.stim);
    fprintf(fid,'\n');
    
-   % Information Line of body
+   % block comment
+   fprintf(fid,'%s %s\t%s\t%s\t%s\t%s\n','%','AX','SIG','AMP','DUR','FREQ');
+   fprintf(fid,'%s\n','%');
+   fprintf(fid,'%s\t%s\t%s\n','%','1','NONE');
+   fprintf(fid,'%s\t%s\t%s\n','%','2','SINE');
+   fprintf(fid,'%s\t%s\t%s\n','%','3','NOISE');
+   fprintf(fid,'%s\t%s\t%s\n','%','4','TURN');
+   fprintf(fid,'%s\t%s\t%s\n','%','5','STEP');
+   fprintf(fid,'%s\t%s\t%s\n','%','6','SUM OF SINES');
+   fprintf(fid,'\n');
+   
+   % trial comment
    fprintf(fid,'%s %s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n','%','MOD','X','Y','ID','INT','On','On','Off','Off','Event');
    fprintf(fid,'%s\t\t\t%s\t%s\t%s\t%s\t%s\t%s\n','%','edg','bit','Event','Time','Event','Time');
    fprintf(fid,'\n');
+   
+   % body line
    fprintf(fid,'%s %s','%%','BODY');
 end
  
