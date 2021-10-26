b100d7535f3b66c986cb185baea26c8adea67af8
Jesse Heckman <j.heckman@science.ru.nl>
2021-10-26 15:52:31 +0200

    bbug

--- setups/vestibular chair/vPrime/cfg/pb_getblock.m
+++ setups/vestibular chair/vPrime/cfg/pb_getblock.m
@@ -40,7 +40,7 @@ function handles = pb_getblock(handles)
    %% CHECK-OUT
    %  Define trial duration, correct stimulus positions, and store handles. 
 
-   if isempty(cfg.trialdur)
+   if isfield(cfg, 'trialdur')
       cfg.trialdur    	= getdurations(block);
    end
    
